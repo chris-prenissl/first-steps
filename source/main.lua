3b7de65a0187ac1503d31f6f73fe5fe76400cdf0
Christoh Prenissl <snakept@icloud.com>
2023-02-11 22:46:58 +0100

    simple movement of sprite

--- source/main.lua
+++ source/main.lua
@@ -3,7 +3,22 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+local pd <const> = playdate
+local gfx <const> = pd.graphics
 
-function playdate.update()
-    print("hello")
+local sprite = nil
+
+local function initialize()
+    local image = gfx.image.new("images/celrage_icon")
+    sprite = gfx.sprite.new(image)
+    sprite:moveTo(200, 120)
+    sprite:add()
+end
+
+initialize()
+
+function pd.update()
+    sprite:moveBy(1,0)
+
+    gfx.sprite.update()    
 end
